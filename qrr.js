54bff0b7b09af1e616ac013fd8f12a8de94f76b0
Montanatech <montanatech16@gmail.com>
2024-09-23 11:02:12 +0200

    Update qrr.js

--- qrr.js
+++ qrr.js
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *MONTNA-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/Montanatech_
+❒ *Author:* _wa.me/+263713347378_
+❒ *YT:* _https://youtube.com/@Kentonx-tech_
 └────────────⭓
 `
 
